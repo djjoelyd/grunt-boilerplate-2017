9561bfd4176f0ae8031087e46caf74c8a8e91401
Joel Delane <joeldelane@gmaill.com>
2017-09-27 11:43:18 +0100

    Initial Commit

---
+++ src/assets/js/modules/module.js
@@ -0,0 +1,15 @@
+module.exports = Module;
+
+function Module() {
+    'use strict';
+
+    var method = {};
+
+    method.init = function() {
+
+    	console.log($);
+
+    };
+
+    return method;
+}
